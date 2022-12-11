86015318eb3677035914f01dca2988974867fbe4
Amina <abajric2@etf.unsa.ba>
2022-12-11 01:00:40 +0100

    added "Book" class that describes a table related to books

---
+++ src/main/java/ba/unsa/etf/rpr/Domain/Book.java
@@ -0,0 +1,85 @@
+package ba.unsa.etf.rpr.Domain;
+
+public class Book {
+    private int bookID;
+    private String title;
+    private String author;
+    private String yearOfPublication;
+    private String genre;
+    private int totalNumber;
+    private int avilableNumber;
+
+    public Book(int bookID, String title, String author, String yearOfPublication, String genre, int totalNumber, int avilableNumber) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+        this.genre = genre;
+        this.totalNumber = totalNumber;
+        this.avilableNumber = avilableNumber;
+    }
+
+    public Book() {
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(String yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public int getAvilableNumber() {
+        return avilableNumber;
+    }
+
+    public void setAvilableNumber(int avilableNumber) {
+        this.avilableNumber = avilableNumber;
+    }
+
+    @Override
+    public String toString() {
+        return title + ", " + author + ", " + yearOfPublication;
+    }
+}
