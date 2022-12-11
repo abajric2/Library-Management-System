4f23734c41f3c4ec9979ceb863af09f8bd44df8e
Amina <abajric2@etf.unsa.ba>
2022-12-11 22:51:44 +0100

    implemented method for searching by author; small changes in searchById method

--- src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
@@ -23,8 +23,28 @@ public class BookDaoSQLImpl implements BookDao {
     }
     @Override
     public List<Book> searchByAuthor(String author) {
-
-        return null;
+        String query = "SELECT * FROM BOOKS WHERE AUTHOR = ?";
+        List<Book> books = new ArrayList<>();
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1, author);
+            ResultSet r = stmt.executeQuery();
+            while(r.next()) {
+                Book book = new Book();
+                book.setBookID(r.getInt("BOOK_ID"));
+                book.setTitle(r.getString("TITLE"));
+                book.setAuthor(r.getString("AUTHOR"));
+                book.setYearOfPublication(r.getString("YEAR_OF_PUBLICATION"));
+                book.setGenre(r.getString("GENRE"));
+                book.setTotalNumber(r.getInt("TOTAL_NUMBER"));
+                book.setAvilableNumber(r.getInt("AVAILABLE_NUMBER"));
+                books.add(book);
+            }
+            r.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
     }
 
     @Override
@@ -66,7 +86,7 @@ public class BookDaoSQLImpl implements BookDao {
 
     @Override
     public Book update(Book item) {
-        String updt = "UPDATE BOOKS SET TITLE = ?, AUTHOR = ?, YEAR_OF_PUBLICATION = ?, GENRE = ?, TOTAL_NUMBER = ?, AVAILABLE_NUMBER = ? WHERE ID = ?";
+        String updt = "UPDATE BOOKS SET TITLE = ?, AUTHOR = ?, YEAR_OF_PUBLICATION = ?, GENRE = ?, TOTAL_NUMBER = ?, AVAILABLE_NUMBER = ? WHERE BOOK_ID = ?";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(updt, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, item.getTitle());
@@ -86,7 +106,7 @@ public class BookDaoSQLImpl implements BookDao {
 
     @Override
     public void delete(Book item) {
-        String dlt = "DELETE FROM BOOK WHERE ID = ?";
+        String dlt = "DELETE FROM BOOK WHERE BOOK_ID = ?";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(dlt, Statement.RETURN_GENERATED_KEYS);
             stmt.setInt(1, item.getBookID());
@@ -98,7 +118,7 @@ public class BookDaoSQLImpl implements BookDao {
 
     @Override
     public Book searchById(int id) {
-        String query = "SELECT * FROM BOOKS WHERE ID = ?";
+        String query = "SELECT * FROM BOOKS WHERE BOOK_ID = ?";
         try {
             PreparedStatement stmt = this.connection.prepareStatement(query);
             stmt.setInt(1, id);
@@ -107,6 +127,7 @@ public class BookDaoSQLImpl implements BookDao {
                 Book book = new Book();
                 book.setBookID(r.getInt("BOOK_ID"));
                 book.setTitle(r.getString("TITLE"));
+                book.setAuthor(r.getString("AUTHOR"));
                 book.setYearOfPublication(r.getString("YEAR_OF_PUBLICATION"));
                 book.setGenre(r.getString("GENRE"));
                 book.setTotalNumber(r.getInt("TOTAL_NUMBER"));
