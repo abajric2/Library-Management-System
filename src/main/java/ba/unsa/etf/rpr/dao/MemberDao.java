42e87f90ded873af02f15105472bf8471b8d3a3e
Amina <abajric2@etf.unsa.ba>
2022-12-11 18:04:50 +0100

    added interface "MemberDao" extended from generic interface "Dao"

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MemberDao.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Member;
+
+import java.util.List;
+
+/**
+ * Interface that extends the generic interface "Dao"
+ * Implements methods specific to working with members of the library
+ * @author Amina Bajric
+ */
+public interface MemberDao extends Dao<Member> {
+    /**
+     * method that returns a list of all members with specified first and last name
+     * @param name library member full name
+     * @return list of members
+     */
+    List<Member> searchByName (String name);
+}
