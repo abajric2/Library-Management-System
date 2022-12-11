4dbd287a94fd43e1486976f680c0aa6ace9b0625
Amina <abajric2@etf.unsa.ba>
2022-12-11 22:53:49 +0100

    implemented method for searching by genre

--- src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDaoSQLImpl.java
@@ -49,7 +49,28 @@ public class BookDaoSQLImpl implements BookDao {
 
     @Override
     public List<Book> searchByGenre(String genre) {
-        return null;
+        String query = "SELECT * FROM BOOKS WHERE GENRE = ?";
+        List<Book> books = new ArrayList<>();
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1, genre);
+            ResultSet r = stmt.executeQuery();
+            while(r.next()) {
+                Book book = new Book();
+                book.setBookID(r.getInt("BOOK_ID"));
+                book.setTitle(r.getString("TITLE"));
+                book.setAuthor(r.getString("AUTHOR"));
+                book.setYearOfPublication(r.getString("YEAR_OF_PUBLICATION"));
+                book.setGenre(r.getString("GENRE"));
+                book.setTotalNumber(r.getInt("TOTAL_NUMBER"));
+                book.setAvilableNumber(r.getInt("AVAILABLE_NUMBER"));
+                books.add(book);
+            }
+            r.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
     }
 
     @Override
