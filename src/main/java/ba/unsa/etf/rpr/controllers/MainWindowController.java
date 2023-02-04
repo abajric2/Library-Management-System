d60b3384195a82bb009bbb83e5992a6c108e949e
Amina <abajric2@etf.unsa.ba>
2023-02-04 17:25:00 +0100

    added files for main window

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainWindowController {
+    public Button profileId;
+    public Button logoutId;
+    public Button profileBttnId;
+    public Button logOutId;
+    public Label welcomeLabel;
+    public Button rentBttn;
+    public Button availableBooksId;
+    public Button rentalsId;
+    public Button helpId;
+
+    public void profileBttnAction(ActionEvent actionEvent) {
+    }
+
+    public void logOutAction(ActionEvent actionEvent) {
+    }
+
+    public void rentAction(ActionEvent actionEvent) {
+    }
+
+    public void availableBooksAction(ActionEvent actionEvent) {
+    }
+
+    public void rentalsAction(ActionEvent actionEvent) {
+    }
+
+    public void helpAction(ActionEvent actionEvent) {
+    }
+}
