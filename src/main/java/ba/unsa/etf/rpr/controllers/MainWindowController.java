cf74b261a6eff1b3a0217997e9be25554ee14e3e
Amina <abajric2@etf.unsa.ba>
2023-02-04 23:01:30 +0100

    added actions for some buttons in main window

--- src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainWindowController.java
@@ -5,10 +5,13 @@ import ba.unsa.etf.rpr.dao.RentalDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Member;
 import ba.unsa.etf.rpr.domain.Rental;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindowController {
     public Button profileBttnId;
@@ -20,7 +23,10 @@ public class MainWindowController {
     public Button byAuthorId;
     public Button byTitleId;
     public Button helpId;
+    public TextField authorNameId;
+    public TextField bookTitleId;
     public Label labelId;
+    public ListView listId;
     private Member memeber;
     MainWindowController(Member m) {
         this.memeber = m;
@@ -58,12 +64,50 @@ public class MainWindowController {
     }
 
     public void allBooksAction(ActionEvent actionEvent) {
-
+        BookDaoSQLImpl b = new BookDaoSQLImpl();
+        List<Book> books = new ArrayList<>(b.getAll());
+        listId.setItems(FXCollections.observableList(books));
     }
 
     public void byAuthorAction(ActionEvent actionEvent) {
+        if(authorNameId.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("The author input field is empty!");
+            alert.setContentText("You must enter the name of the author whose books you want to view!");
+            alert.showAndWait();
+            return;
+        }
+        BookDaoSQLImpl b = new BookDaoSQLImpl();
+        List<Book> books = new ArrayList<>(b.searchByAuthor(authorNameId.getText()));
+        if(books == null || books.size() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("No books found!");
+            alert.setContentText("No book was found by the author you entered!");
+            alert.showAndWait();
+        }
+        else listId.setItems(FXCollections.observableList(books));
     }
 
     public void byTitleAction(ActionEvent actionEvent) {
+        if(bookTitleId.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("The author input field is empty!");
+            alert.setContentText("You must enter the name of the author whose books you want to view!");
+            alert.showAndWait();
+            return;
+        }
+        BookDaoSQLImpl b = new BookDaoSQLImpl();
+        List<Book> books = new ArrayList<>(b.searchByTitle(bookTitleId.getText()));
+        if(books == null || books.size() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("No books found!");
+            alert.setContentText("No book was found with the title you entered!");
+            alert.showAndWait();
+        }
+        else listId.setItems(FXCollections.observableList(books));
     }
 }
