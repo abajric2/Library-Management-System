a581877b845b5afef57e61275b4862dd55555618
Amina <abajric2@etf.unsa.ba>
2023-02-24 18:56:30 +0100

    added library exception

--- src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
@@ -1,11 +1,11 @@
 package ba.unsa.etf.rpr.exceptions;
 
 public class LibraryException extends Exception {
-    public LibraryException(String msg, Exception reason){
-        super(msg, reason);
+    public LibraryException(String message, Exception reason){
+        super(message, reason);
     }
 
-    public LibraryException(String msg){
-        super(msg);
+    public LibraryException(String message){
+        super(message);
     }
 }
