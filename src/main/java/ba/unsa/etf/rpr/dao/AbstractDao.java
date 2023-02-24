515e73b253fc16b6e7a87fb99fbcbf2130dc15d1
Amina <abajric2@etf.unsa.ba>
2023-02-24 19:03:19 +0100

    AbstractDao class; implemented constructor and method for creating a connection

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.LibraryException;
+
+import java.sql.*;
+import java.util.*;
+
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+    private static Connection connection = null;
+    private String table;
+
+    public AbstractDao(String table) {
+        this.table = table;
+        createConnection();
+    }
+
+    private static void createConnection(){
+        if(AbstractDao.connection==null) {
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                AbstractDao.connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally {
+                Runtime.getRuntime().addShutdownHook(new Thread(){
+                    @Override
+                    public void run(){
+                        try {
+                            connection.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+}
