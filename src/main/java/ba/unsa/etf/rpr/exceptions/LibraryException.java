04062871e62c47cbaf5776a97d4c1c4916e5f3c2
Amina <abajric2@etf.unsa.ba>
2023-02-24 18:49:43 +0100

    added library exception

---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.exceptions;
+
+public class LibraryException extends Exception {
+    public LibraryException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public LibraryException(String msg){
+        super(msg);
+    }
+}
