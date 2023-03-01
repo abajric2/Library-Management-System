09322760d6a4276bcabb029b62ad8e6c8fa8ee1a
Amina <abajric2@etf.unsa.ba>
2023-03-02 00:45:46 +0100

    update books

--- src/main/java/ba/unsa/etf/rpr/controllers/ManageBooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ManageBooksController.java
@@ -11,10 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class ManageBooksController {
     public Button allBooksId;
@@ -35,6 +32,11 @@ public class ManageBooksController {
     public TextField yearLabel;
     public TextField genreLabel;
     public TextField totalNumberLabel;
+    public Button updateBttn;
+    public Button addBttn;
+    public TextField availableNumberLabel;
+    private BookModel model = new BookModel();
+    private Integer idUpdate;
 
     @FXML
     public void initialize() throws LibraryException {
@@ -80,6 +82,30 @@ public class ManageBooksController {
             }
         });
       //  tableId.getSelectionModel().getSelectedItem();
+        tableId.getSelectionModel().selectedItemProperty().addListener((obs,o,n)->{
+            if(o!=null){
+                Book b = (Book) o;
+                idUpdate = b.getId();
+                titleLabel.textProperty().unbindBidirectional(model.title);
+                authorLabel.textProperty().unbindBidirectional((model.author));
+                yearLabel.textProperty().unbindBidirectional(model.yearOfPublication);
+                genreLabel.textProperty().unbindBidirectional(model.genre);
+                totalNumberLabel.textProperty().unbindBidirectional(model.totalNumber);
+                availableNumberLabel.textProperty().unbindBidirectional(model.availableNumber);
+            }
+            Book b = (Book) n;
+            if(b != null) {
+                idUpdate = b.getId();
+                model.fromBook((Book) n);
+                titleLabel.textProperty().bindBidirectional(model.title);
+                authorLabel.textProperty().bindBidirectional(model.author);
+                yearLabel.textProperty().bindBidirectional(model.yearOfPublication);
+                genreLabel.textProperty().bindBidirectional(model.genre);
+                totalNumberLabel.textProperty().bindBidirectional(model.totalNumber);
+                availableNumberLabel.textProperty().bindBidirectional(model.availableNumber);
+            }
+        });
+
     }
     public void allBooks(ActionEvent actionEvent) throws LibraryException {
         id.setCellValueFactory(cellData->{Book book=cellData.getValue(); return new SimpleIntegerProperty(book.getId()).asObject();});
@@ -99,5 +125,95 @@ public class ManageBooksController {
     }
 
     public void searchByGenre(ActionEvent actionEvent) {
+    }
+
+    public void updateAction(ActionEvent actionEvent) throws LibraryException {
+        if(idUpdate == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Update error!");
+            alert.setContentText("Choose the book you want to edit in the table!");
+            alert.showAndWait();
+            return;
+        }
+        Book b = new Book();
+        b.setId(idUpdate);
+        b.setTitle(model.title.get());
+        b.setAuthor(model.author.get());
+        b.setYearOfPublication(model.yearOfPublication.get());
+        b.setGenre(model.genre.get());
+        b.setTotalNumber(Integer.parseInt(model.totalNumber.get()));
+        b.setAvilableNumber(Integer.parseInt(model.availableNumber.get()));
+        try {
+            manager.update(b);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Successfully updated!");
+            alert.showAndWait();
+            tableId.setItems(FXCollections.observableList(manager.getAll()));
+            idUpdate = null;
+        } catch (LibraryException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Update error!");
+            alert.setContentText("You can't update this book!");
+            alert.showAndWait();
+        }
+    }
+
+    public void addAction(ActionEvent actionEvent) {
+        Book b = new Book();
+        b.setId(1);
+        b.setTitle(model.title.get());
+        b.setAuthor(model.author.get());
+        b.setYearOfPublication(model.yearOfPublication.get());
+        b.setGenre(model.genre.get());
+        b.setTotalNumber(Integer.parseInt(model.totalNumber.get()));
+        b.setAvilableNumber(Integer.parseInt(model.availableNumber.get()));
+        try {
+            manager.add(b);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Successfully added!");
+            alert.showAndWait();
+            tableId.setItems(FXCollections.observableList(manager.getAll()));
+        } catch (LibraryException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("You can't add book!");
+            alert.setContentText("You can't add book!");
+            alert.showAndWait();
+        }
+    }
+
+    public class BookModel {
+        public SimpleStringProperty title = new SimpleStringProperty("");
+        public SimpleStringProperty author = new SimpleStringProperty("");
+        public SimpleStringProperty yearOfPublication = new SimpleStringProperty("");
+        public SimpleStringProperty genre = new SimpleStringProperty("");
+        public SimpleStringProperty totalNumber = new SimpleStringProperty();
+        public SimpleStringProperty availableNumber = new SimpleStringProperty();
+
+        public void fromBook (Book b){
+            this.title.set(b.getTitle());
+            this.author.set(b.getAuthor());
+            this.yearOfPublication.set(b.getYearOfPublication());
+            this.genre.set(b.getGenre());
+            this.totalNumber.set(Integer.toString(b.getTotalNumber()));
+            this.availableNumber.set(Integer.toString(b.getAvilableNumber()));
+        }
+
+        public Book toBook(){
+            Book b = new Book();
+            b.setTitle(this.title.getValue());
+            b.setAuthor(this.author.getValue());
+            b.setYearOfPublication(this.yearOfPublication.getValue());
+            b.setGenre(this.genre.getValue());
+            b.setTotalNumber(Integer.parseInt(totalNumber.get()));
+            b.setAvilableNumber(Integer.parseInt(availableNumber.get()));
+            return b;
+        }
     }
 }
