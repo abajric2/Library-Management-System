fe7e4e39c55f0c52c014bc79efeccd74295309e8
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-08-15 15:44:00 +0200

    testing add method and searchById

--- src/test/java/ba/unsa/etf/rpr/Testing.java
+++ src/test/java/ba/unsa/etf/rpr/Testing.java
@@ -12,4 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Testing {
 
+    private BookManager bookManager;
+    private Book testBook;
+
+    @BeforeEach
+    public void setup() {
+        bookManager = new BookManager();
+        testBook = new Book();
+        testBook.setId(1);
+        testBook.setTitle("Test Book");
+        testBook.setAuthor("Test Author");
+        testBook.setYearOfPublication("2023");
+        testBook.setGenre("Test Genre");
+        testBook.setTotalNumber(100);
+        testBook.setAvailableNumber(90);
+    }
+
+    @Test
+    public void testAddAndSearchById() throws LibraryException {
+        Book addedBook = bookManager.add(testBook);
+        Book foundBook = bookManager.searchById(addedBook.getId());
+        assertEquals(addedBook, foundBook, "Book not found by id!");
+        bookManager.delete(addedBook);
+    }
 }
