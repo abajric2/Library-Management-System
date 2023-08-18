218c2d1f40edde58d0f516f076d2e6253e5bbd4d
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-08-18 19:43:28 +0200

    added dependency for mockito tests

---
+++ src/test/java/ba/unsa/etf/rpr/business/MockTesting.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.MemberDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+public class MockTesting {
+
+}
